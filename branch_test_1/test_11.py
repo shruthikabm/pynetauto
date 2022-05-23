b50ba46a2d5489f6b46bcc483c054bd8a4af091f
Shruthika BM <bm.shruthika@gmail.com>
2022-05-23 16:51:32 -0700

    commit branch test directories

---
+++ branch_test_1/test_11.py
@@ -0,0 +1,9 @@
+my_dict = { "fruit" : "Grapes",
+            "veggie" : "Carrot",
+            "nut" : "Almonds"
+          }
+
+for key, value in my_dict.items():
+    print(key, "--->", value)
+
+
